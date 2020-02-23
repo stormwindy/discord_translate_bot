c5af9d07231ed3899b0a95af787de5d2fc5e8811
Ege E <ege@elgun.xyz>
2020-02-23 23:54:20 +0000

    lang support

--- t2s.py
+++ t2s.py
@@ -2,10 +2,10 @@ from gtts import gTTS
 import os,sys
 
 myText = ''
-for i in range(1, len(sys.argv)):
+for i in range(2, len(sys.argv)):
     myText += sys.argv[i]
 # myText = str(sys.argv[1])
-language = 'tr'
+language = sys.argv[1]
 output = gTTS(text=myText, lang = language, slow=False)
 output.save("output.mp3")
 os.system("start output.mp3")
